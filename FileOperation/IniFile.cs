39ea43bc6f15d6f2bbd8650c84abfa3e61bbbb8c
ncl5722354 <897117296@qq.com>
2021-06-04 16:16:14 +0800

    2021 06 04

---
+++ FileOperation/IniFile.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Collections;
+
+// ini操作
+// [DllImport("kernel32")] 
+// private static extern long WritePrivateProfileString(string section, string key, string val, string filePath); 
+// section: 要写入的段落名
+// key: 要写入的键，如果该key存在则覆盖写入
+// val: key所对应的值
+// filePath: INI文件的完整路径和文件名
+
+
+namespace FileOperation
+{
+    public class IniFile
+    {
+        public string Path;
+
+        // 声明INI文件的写操作函数 WritePrivateProfileString()
+        [System.Runtime.InteropServices.DllImport("kernel32")]
+        public static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+
+        public IniFile(string path)
+        {
+            this.Path = path;
+            try
+            {
+                FileCaozuo.Create_File(path); // 尝试创建这个文件
+            }
+            catch { }
+        }
+        // 声明INI文件的读操作函数 GetPrivateProfileString()
+
+        [System.Runtime.InteropServices.DllImport("kernel32")]
+
+        public static extern int GetPrivateProfileString(string section, string key, string def, System.Text.StringBuilder retVal, int size, string filePath);
+       
+        [System.Runtime.InteropServices.DllImport("kernel32")]
+        public static extern int GetPrivateProfileSectionNames(byte[] buffer, int iLen, string lpFileName);
+
+        /// <summary>
+        /// 写INI文件
+        /// </summary>
+        /// <param name="section">段落</param>
+        /// <param name="key">键</param>
+        /// <param name="iValue">值</param>
+        public void IniWriteValue(string section, string key, string iValue) 
+        {
+            WritePrivateProfileString(section, key, iValue, this.Path);
+        }
+
+        /// <summary>
+        /// 读取INI文件
+        /// </summary>
+        /// <param name="section">段落</param>
+        /// <param name="key">键</param>
+        /// <returns>返回的键值</returns>
+        public string IniReadValue(string section, string key) 
+        { 
+            StringBuilder temp = new StringBuilder(255); 
+
+            int i = GetPrivateProfileString(section, key, "", temp, 255, this.Path); 
+            return temp.ToString();
+        }
+
+
+
+        public ArrayList ReadSections()
+        {
+            byte[] buffer = new byte[65535];
+            int rel = GetPrivateProfileSectionNames(buffer, buffer.GetUpperBound(0), this.Path);
+            return Conver2ArrayList(rel, buffer);
+        }
+        public ArrayList Conver2ArrayList(int rel, byte[] buffer)
+        {
+            ArrayList arrayList = new ArrayList();
+            if (rel > 0)
+            {
+                int iCnt, iPos;
+                string tmp;
+                iCnt = 0; iPos = 0;
+                for (iCnt = 0; iCnt < rel; iCnt++)
+                {
+                    if (buffer[iCnt] == 0x00)
+                    {
+                        tmp = System.Text.ASCIIEncoding.Default.GetString(buffer, iPos, iCnt - iPos).Trim();
+                        iPos = iCnt + 1;
+                        if (tmp != "")
+                            arrayList.Add(tmp);
+                    }
+                }
+            }
+            return arrayList;
+        }
+
+
+        /// <summary>
+        /// 读取INI文件
+        /// </summary>
+        /// <param name="Section">段，格式[]</param>
+        /// <param name="Key">键</param>
+        /// <returns>返回byte类型的section组或键值组</returns>
+        //public byte[] IniReadValues(string section, string key)
+        //{
+        //    byte[] temp = new byte[255];
+
+        //    int i = GetPrivateProfileString(section,key,"", temp,255, this.Path);
+        //    return temp;
+        //}
+    }
+}
---
+++ SaigeSystemShow/privatefunction.js
@@ -0,0 +1,164 @@
+﻿
+
+
+// 工程需要的私有函数
+
+function Read_Selector()
+{
+    var result;
+    $.ajax({
+        url: "SqlCaozuo.asmx/ReadSections",
+        type: "Post",
+        dataType: "text",
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        data: "{ini_path_name:'viewconfig.ini'}",
+        success: function (data) {
+            result = data;
+
+        },
+        error: function (data) {
+            //200的响应也有可能被认定为error，responseText中没有Message部分
+            return $.parseJSON(data.responseText).Message;
+        },
+        complete: function (data) {
+
+        }
+    });
+    return result;                                                                                  
+}
+
+
+// 读取所有的界面
+function Read_View(view_name,subdiv_name)
+{
+    var all_object_string;             // 所有的界面元素的字符串
+    var ini_name = view_name + ".ini";
+
+    var subdiv = document.getElementById(subdiv_name);
+    subdiv.innerHTML = "";
+    $.ajax({
+        url: "SqlCaozuo.asmx/ReadSections",
+        type: "Post",
+        dataType: "text",
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        data: "{ini_path_name:'"+ini_name+"'}",
+        success: function (data) {
+            all_object_string = data;
+
+        },
+        error: function (data) {
+            //200的响应也有可能被认定为error，responseText中没有Message部分
+            return $.parseJSON(data.responseText).Message;
+        },
+        complete: function (data) {
+
+        }
+    });
+
+    var all_object_json = From_Text_To_Json(all_object_string);
+
+    for(var i=0;i<all_object_json.length;i++)
+    {
+        var object_name = all_object_json[i];               // 元素名称  seciton
+
+
+        // 在这里做界面的解析
+        // 
+        var mydiv;
+
+        // 各种 元素的类型
+        var type = Read_Value(ini_name, object_name, "类型");
+        if(type=="边框1")
+        {
+            mydiv = document.createElement("div");
+            mydiv.style.position = "absolute";
+            
+            var myimage = document.createElement("img");
+            myimage.style.width = "100%";
+            myimage.style.height = "100%";
+            myimage.style.left = "0%";
+            myimage.style.top = "0%";
+            myimage.style.position = "absolute";
+            myimage.src = "pic/frame1.png";         //src="pic/frame1.png"
+            mydiv.appendChild(myimage);
+
+            var myimage2 = document.createElement("img");
+            myimage2.style.width = "100%";
+            myimage2.style.height = "90%";
+            myimage2.style.left = "0%";
+            myimage2.style.top = "10%";
+            myimage2.style.position = "absolute";
+            myimage2.src = "pic/frame2.png";
+            mydiv.appendChild(myimage2);
+        }
+
+        if(type=="")
+        {
+            break;
+        }
+
+        // 父结点属性 
+        var parent = Read_Value(ini_name, object_name, "父结点")
+        if(parent=="")
+        {
+            var sub = document.getElementById(subdiv_name);
+            sub.appendChild(mydiv);
+        }
+
+        var width = Read_Value(ini_name, object_name, "width");
+
+        if(width!="")
+        {
+            mydiv.style.width = width;
+        }
+
+        var left = Read_Value(ini_name, object_name, "left");
+        if(left!="")
+        {
+            mydiv.style.left = left;
+        }
+
+        var top = Read_Value(ini_name, object_name, "top")
+        if (top != "")
+        {
+            mydiv.style.top = top;
+        }
+
+        var height = Read_Value(ini_name, object_name, "height")
+        if (height != "")
+        {
+            mydiv.style.height = height;
+        }
+        
+    }
+}
+
+
+function Read_Value(ini_name,section_name,key)
+{
+    var value_string;
+    $.ajax({
+        url: "SqlCaozuo.asmx/Read_Init",
+        type: "Post",
+        dataType: "text",
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        data: "{ini_path_name:'" + ini_name + "',section:'" + section_name + "',key:'"+key + "'}",
+        success: function (data) {
+            value_string = data;
+
+        },
+        error: function (data) {
+            //200的响应也有可能被认定为error，responseText中没有Message部分
+            return $.parseJSON(data.responseText).Message;
+        },
+        complete: function (data) {
+
+        }
+    });
+
+    var value_json = From_Text_To_Json(value_string);
+    return value_json;
+}
